0954d1748a6e553dbec6fe64d447a6b2a0370052
juniorpacajes1 <135669834+juniorpacajes1@users.noreply.github.com>
2026-09-20 01:51:11 -0400

    Add files via upload

---
+++ Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Contexto
+    {
+        public dbDesarrolloSistemaISeptiembre2026Entities TraerContexto()
+        {
+            dbDesarrolloSistemaISeptiembre2026Entities con = new dbDesarrolloSistemaISeptiembre2026Entities();
+            return con;
+        }
+    }
+}
